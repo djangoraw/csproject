d33e900cc28e57f541b0c570cf5d0a82a4099f7a
Ayenar <pretty.v4cant@gmail.com>
2020-06-24 17:06:24 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
